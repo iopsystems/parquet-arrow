fb797be2f8028f0365fa257f925b6447e2cd7fd7
Mihir Nanavati <code@mihirnanavati.com>
2024-04-05 11:58:31 -0400

    support displaying schema and converting parquet files

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use arrow::csv::writer::Writer;
 use arrow::datatypes::*;
 use arrow::ipc::writer::{FileWriter, IpcWriteOptions};
 use arrow::ipc::CompressionType;
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use parquet::arrow::arrow_reader::ParquetRecordBatchReaderBuilder;
 use parquet::arrow::ArrowWriter;
 use parquet::basic::*;
@@ -16,16 +16,38 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::sync::Arc;
 
+#[derive(Subcommand)]
+enum Commands {
+    Generate {
+        #[arg(short, long)]
+        compression: bool,
+
+        #[arg(short, long)]
+        list: bool,
+
+        #[arg(short, long)]
+        array: bool,
+
+        #[arg(short, long)]
+        keepfile: bool,
+    },
+    Schema {
+        #[arg(short, long)]
+        input_file: String,
+    },
+    Convert {
+        #[arg(short, long)]
+        input_file: String,
+
+        #[arg(short, long)]
+        compression: bool,
+    },
+}
+
 #[derive(Parser)]
 struct CliArgs {
-    #[arg(short, long)]
-    compression: bool,
-
-    #[arg(short, long)]
-    listarray: bool,
-
-    #[arg(short, long)]
-    keepfile: bool,
+    #[command(subcommand)]
+    command: Commands,
 }
 
 const STRINGS: &[&str] = &["foo", "bar", "baz"];
@@ -69,7 +91,7 @@ fn build_string_column(
     pq_series.push(series);
 }
 
-fn build_u64_array_column(
+fn build_u64_list_column(
     name: &str,
     data: Vec<Vec<u64>>,
     pq_cols: &mut Vec<Field>,
@@ -86,9 +108,33 @@ fn build_u64_array_column(
     pq_series.push(Arc::new(builder.finish()));
 }
 
+fn build_u64_array_column(
+    name: &str,
+    data: Vec<Vec<u64>>,
+    pq_cols: &mut Vec<Field>,
+    pq_series: &mut Vec<Arc<dyn Array>>,
+) {
+    let h = HashMap::from([("type".to_owned(), "u64_array".to_owned())]);
+    let mut builder = FixedSizeListBuilder::new(UInt64Builder::new(), 3);
+    let field = Field::new(
+        name,
+        DataType::FixedSizeList(Arc::new(Field::new("item", DataType::UInt64, true)), 3),
+        false,
+    )
+    .with_metadata(h);
+
+    for inner in data {
+        builder.values().append_slice(&inner);
+        builder.append(true);
+    }
+    pq_cols.push(field);
+    pq_series.push(Arc::new(builder.finish()));
+}
+
 fn write_parquet(
     output: String,
-    listarray: bool,
+    list: bool,
+    array: bool,
     compression: bool,
     schema: SchemaRef,
     nrows: u8,
@@ -107,20 +153,13 @@ fn write_parquet(
 
     let mut writer = ArrowWriter::try_new(file, schema, Some(props)).unwrap();
     for _ in 0..nbatches {
-        let batch = generate_batch(nrows, listarray);
+        let batch = generate_batch(nrows, list, array);
         writer.write(&batch).unwrap();
     }
     writer.close().unwrap();
 }
 
-fn write_ipc(
-    output: String,
-    listarray: bool,
-    compression: bool,
-    schema: SchemaRef,
-    nrows: u8,
-    nbatches: u8,
-) {
+fn write_ipc(output: String, compression: bool, batch: &RecordBatch) {
     let file = File::create(output).unwrap();
 
     let opts = match compression {
@@ -130,11 +169,8 @@ fn write_ipc(
             .unwrap(),
     };
 
-    let mut writer = FileWriter::try_new_with_options(file, &schema, opts).unwrap();
-    for _ in 0..nbatches {
-        let batch = generate_batch(nrows, listarray);
-        writer.write(&batch).unwrap();
-    }
+    let mut writer = FileWriter::try_new_with_options(file, &batch.schema(), opts).unwrap();
+    writer.write(batch).unwrap();
     writer.close().unwrap();
 }
 
@@ -145,11 +181,20 @@ fn write_csv(output: String, batch: &RecordBatch) {
     writer.close().unwrap();
 }
 
-fn generate_data(nrows: u8) -> (Vec<String>, Vec<u64>, Vec<f64>, Vec<Vec<u64>>) {
+fn generate_data(
+    nrows: u8,
+) -> (
+    Vec<String>,
+    Vec<u64>,
+    Vec<f64>,
+    Vec<Vec<u64>>,
+    Vec<Vec<u64>>,
+) {
     let mut rng = rand::thread_rng();
     let mut strings: Vec<String> = Vec::new();
     let mut nums: Vec<u64> = Vec::new();
     let mut floats: Vec<f64> = Vec::new();
+    let mut lists: Vec<Vec<u64>> = Vec::new();
     let mut arrays: Vec<Vec<u64>> = Vec::new();
 
     for _ in 0..nrows {
@@ -161,22 +206,26 @@ fn generate_data(nrows: u8) -> (Vec<String>, Vec<u64>, Vec<f64>, Vec<Vec<u64>>) 
         for j in 0..3 {
             inner[j] = rng.gen_range(0..25);
         }
+        lists.push(inner.clone());
         arrays.push(inner);
     }
 
-    return (strings, nums, floats, arrays);
+    return (strings, nums, floats, lists, arrays);
 }
 
-fn generate_batch(nrows: u8, listarray: bool) -> RecordBatch {
+fn generate_batch(nrows: u8, list: bool, array: bool) -> RecordBatch {
     let mut pq_cols: Vec<Field> = Vec::new();
     let mut pq_series: Vec<Arc<dyn Array>> = Vec::new();
 
-    let (strings, nums, floats, arrays) = generate_data(nrows);
+    let (strings, nums, floats, lists, arrays) = generate_data(nrows);
 
     build_u64_column("nums", nums, &mut pq_cols, &mut pq_series);
     build_f64_column("floats", floats, &mut pq_cols, &mut pq_series);
     build_string_column("strings", strings, &mut pq_cols, &mut pq_series);
-    if listarray {
+    if list {
+        build_u64_list_column("lists", lists, &mut pq_cols, &mut pq_series);
+    }
+    if array {
         build_u64_array_column("arrays", arrays, &mut pq_cols, &mut pq_series);
     }
 
@@ -186,33 +235,30 @@ fn generate_batch(nrows: u8, listarray: bool) -> RecordBatch {
     return batch;
 }
 
-fn build_arrow(output: &str, nrows: u8, compression: bool, listarray: bool) {
-    let batch = generate_batch(nrows, listarray);
+fn build_arrow(output: &str, nrows: u8, compression: bool, list: bool, array: bool) {
+    let batch = generate_batch(nrows, list, array);
     write_parquet(
         output.to_owned() + ".parquet",
-        listarray,
+        list,
+        array,
         compression,
         batch.schema(),
         nrows,
         2,
     );
-
-    // write_ipc(
-    //     output.to_owned() + ".ipc",
-    //     listarray,
-    //     compression,
-    //     batch.schema(),
-    //     nrows,
-    //     2,
-    // );
-    // write_csv(output.to_owned() + ".csv", &batch);
 }
 
-fn read_parquet(input: &str) {
-    let file = File::open(input.to_owned() + ".parquet").unwrap();
+fn read_parquet_batch(input: &str) -> RecordBatch {
+    let file = File::open(input).unwrap();
     let builder = ParquetRecordBatchReaderBuilder::try_new(file).unwrap();
     let mut reader = builder.build().unwrap();
     let record_batch = reader.next().unwrap().unwrap();
+    return record_batch;
+}
+
+fn read_parquet(input: &str) {
+    let name = input.to_owned() + ".parquet";
+    let record_batch = read_parquet_batch(&name);
     for i in 0..record_batch.num_columns() {
         let col = &record_batch.columns()[i];
         println!("Column {}: {:#?}", i, col);
@@ -221,13 +267,36 @@ fn read_parquet(input: &str) {
 
 fn main() {
     let args = CliArgs::parse();
-    let name = "foobar";
-    build_arrow(name, 3, args.compression, args.listarray);
-    read_parquet(name);
 
-    if !args.keepfile {
-        let _ = std::fs::remove_file(name.to_owned() + ".parquet");
-        // let _ = std::fs::remove_file(name.to_owned() + ".ipc");
-        // let _ = std::fs::remove_file(name.to_owned() + ".csv");
+    match &args.command {
+        Commands::Generate {
+            compression,
+            list,
+            array,
+            keepfile,
+        } => {
+            let name = "foobar";
+            build_arrow(name, 3, *compression, *list, *array);
+            read_parquet(name);
+
+            if !keepfile {
+                let _ = std::fs::remove_file(name.to_owned() + ".parquet");
+            }
+        }
+        Commands::Schema { input_file } => {
+            let batch = read_parquet_batch(input_file);
+            let schema = batch.schema();
+            for f in schema.fields() {
+                println!("{}. {:#?}, {:#?}", f.name(), f.data_type(), f.metadata());
+            }
+        }
+        Commands::Convert {
+            input_file,
+            compression,
+        } => {
+            let output = input_file.to_owned() + ".arrow";
+            let batch = read_parquet_batch(input_file);
+            write_ipc(output, *compression, &batch);
+        }
     }
 }
